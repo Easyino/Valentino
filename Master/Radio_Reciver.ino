7e0130408df1d75d4eedfee5cfc163dfe3c1d80a
fededelong03 <65185337+fededelong03@users.noreply.github.com>
2021-02-03 21:12:58 +0100

    basic reciver code

--- Master/Radio_Reciver.ino
+++ Master/Radio_Reciver.ino
@@ -1,1 +1,28 @@
+void radioConfigRec() {
+  
+  int ret = 10;
+  while (!radio.begin()) {
+    Serial.println(F("radio hardware is not responding!!"));
+    ret--;
+    delay(500);
+    if (ret < 0)    while (1) {}
+  }
+  
+  radio.setPALevel(RF24_PA_LOW);
+  radio.setPayloadSize(sizeof(payload));
+  radio.openWritingPipe(address[0]);
+  radio.openReadingPipe(1, address[1]);
+  radio.startListening();
+  while (!getRadioMessage());
+}
 
+bool getRadioMessage() {
+  uint8_t pipe;
+  if (radio.available(&pipe)) {
+    uint8_t bytes = radio.getPayloadSize();
+    radio.read(&payload, bytes);
+    Serial.println(payload);
+    return true;
+  }
+  return false;
+}
--- Master/settings.h
+++ Master/settings.h
@@ -1,15 +1,6 @@
 // Radio connection
 RF24 radio(7, 8); 
-
-uint8_t address[][6] = {"1Node", "2Node"};
-
-
-bool radioNumber = 1; // 0 uses address[0] to transmit, 1 uses address[1] to transmit
-
-
-
-bool role = false;  // true = TX role, false = RX role
-
-
-
+uint8_t address[][6] = {"Valen1","Valen2"};
 float payload = 0.0;
+#define TIMEOUT_BUFFER 2000
+long int radio_timeout=TIMEOUT_BUFFER;
