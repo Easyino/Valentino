f0e521ef1cf2328e6754673a5e9ee5d4b72431bc
pilotto8 <66770848+pilotto8@users.noreply.github.com>
2021-02-03 23:57:58 +0100

    Temporaneous sketch

---
+++ Prova_infrarossi/Prova_infrarossi.ino
@@ -0,0 +1,43 @@
+#include <alpha.h>
+Easy scheda(true);
+#define delta 2
+#define period 150
+#define averageLength 100
+int counter = 0;
+bool passed = false;
+unsigned long int previousMillisPulse;
+unsigned int pulse[averageLength];
+float average = 0;
+int i;
+void setup() {
+  Serial.begin(250000);
+  pinMode(A1, INPUT);
+  previousMillisPulse = millis();
+}
+
+void loop() {
+  Serial.print(pulse[counter]);
+  Serial.print(" | ");
+  Serial.println(average - 5);
+  Serial.print(" | ");
+  Serial.println(passed - 10);
+  Serial.print(" | ");
+  Serial.println(-10);
+  average *= averageLength;
+  average -= pulse[counter];
+  pulse[counter] = analogRead(A1);
+  average += pulse[counter];
+  average /= averageLength;
+  if (pulse[counter] - average >= delta){
+    previousMillisPulse = millis();
+  } 
+  counter = (counter + 1) % averageLength;
+  if (millis() - previousMillisPulse > period){
+    passed = true;
+    scheda.accendi(0, 50, 0);
+  }
+  else {
+    passed = false;
+    scheda.accendi(0, 0, 0);
+  }
+}
